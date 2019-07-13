676fab01ee5496cde3027918ccfdc1505f812113
Sergey Fokin <sfokin@live.com>
2019-07-14 02:20:46 +0300

    its fly and have sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+	const float rscThrust = 1f;
+	Rigidbody rigidBody;
+	AudioSource audioSource;
+
 	// Use this for initialization
 	void Start () {
-		
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -15,19 +20,29 @@ public class Rocket : MonoBehaviour {
         ProcessInput();
     }
 
-    private static void ProcessInput()
+    private void ProcessInput()
     {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			audioSource.Play();
+		}
+
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
+
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			audioSource.Stop();
+		}
 
 		if(Input.GetKey(KeyCode.A))
 		{
-			print("Rotate Left");
+			transform.Rotate(Vector3.forward );// * Time.deltaTime);
 		} else if(Input.GetKey(KeyCode.D))
 		{
-			print("Rotate Right");
+			transform.Rotate(-Vector3.forward);//  * Time.deltaTime);
 		}
     }
 }
